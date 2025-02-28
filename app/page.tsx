8c6077682a479d8e3843a552e139f642aee1e7c5
NoelG2002 <132538006+NoelG2002@users.noreply.github.com>
2025-02-28 14:35:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-4">AgriVaani</h1>
       <ThemeToggle theme={theme} setTheme={setTheme} />
 
-      <div className="w-full max-w-[800px]  bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 flex space-x-4">
+<div className="w-full max-w-[900px] ml-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 flex space-x-4">
         {/* Left Section: Input */}
         <div className="w-1/2 flex flex-col space-y-4">
           <div className="flex space-x-2">
@@ -233,7 +233,7 @@ export default function Home() {
       </div>
 
       {/* ASR Section */}
-      <div className="mt-6 w-full max-w-[800px] bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6">
+<div className="w-full max-w-[1000px] ml-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 flex space-x-4">
         <h2 className="text-lg font-semibold mb-2">Speech Recognition (ASR)</h2>
         <input
           type="file"
